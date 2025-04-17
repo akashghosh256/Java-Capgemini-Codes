b99f495eae7ae65e990b9fc6e6db366ffb64000e
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2025-04-17 14:28:20 +0530

    Add files via upload

---
+++ LinkedListPack/Drive.java
@@ -0,0 +1,14 @@
+package LinkedListPack;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Drive {
+	public static void main(String[] args) {
+		List<Integer> ar = new LinkedList<>();
+		ar.add(56);
+		for(int i : ar)
+			System.out.println(i); 
+	}
+
+}
